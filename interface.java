165ae5455066ee89f229cf81e2d9c63998085716
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-07-13 10:09:35 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,11 @@
+interface Animal {
+  void sound();
+}
+class Dog implement Animal {
+  public void sound() {
+    system.out.println("bark");
+public static void main(string[]  args)
+  Dog d= new Dog();
+  d.sound();
+  }
+}
