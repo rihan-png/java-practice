8463a86acc8f96e5db8327760cac73ab90d6217f
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-07-13 09:10:57 +0530

    Implement method overloading in Calc class
    Added method overloading for addition in Calc class.

---
+++ methodoverlaoig.java
@@ -0,0 +1,13 @@
+Class Calc
+  int add(int a,int b) {
+  return a+b;
+}
+int add(int a, int b, int c) {
+  return a+b+c;
+  public static void main(string[] agrs) {
+    Calc c = new  Calc()
+  system.out.println(c.add(10,20));
+    system.out.println(c.add(23,21,78));
+  }
+}
+    
