923893d916af3b41aac89f8e61121f19d4b3773e
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-07-14 11:00:32 +0530

    Add default constructor to student class

---
+++ constructor.java
@@ -0,0 +1,10 @@
+//Constructor is a special method that automatically runs when an object is created.
+class student
+ // This is a default (no-argument) constructor. 
+    // It is automatically called when an object is instantiated (e.g., Student s = new Student();)
+  {
+    student()
+    {
+      system.out.println("created");
+    }
+  
